10364597392bf6028894df2de793aa31619361ee
Dipayan2331 <146375093+Dipayan2331@users.noreply.github.com>
2024-07-16 14:46:32 +0000

    datatype

---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,8 @@
+"use strict" //treat all js in new version
+// alert(3+3) // we r using nodejs not browser
+console.log(3+3)
+console.log("hitesh")
+let name= "dip"
+let age= 7
+let isloggedIn = false
+console.log(typeof"dip")
