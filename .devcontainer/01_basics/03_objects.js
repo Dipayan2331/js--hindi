4bb44b1f2b1ac38ece196b060557bf34f3d3d53e
Dipayan2331 <146375093+Dipayan2331@users.noreply.github.com>
2024-08-08 07:05:29 +0000

    push

---
+++ .devcontainer/01_basics/03_objects.js
@@ -0,0 +1,5 @@
+//singleton
+//object literals
+const jsUser={}
+//Object.create 
+myArray=["f","y"]
---
+++ .devcontainer/01_basics/array.js
@@ -0,0 +1,6 @@
+const marvel= ["thor","spiderman","ironman"]
+const dc= ['superman',"flash","batman"]
+marvel.push(dc)
+marvel.concat(dc)
+console.log(marvel);
+
